8fdebc343afd2a3783cfb8a4e4b572cfb49be011
jaypoojara <jaypoojara@gmail.com>
2025-01-13 18:54:15 +0530

    feat: integrate nyt most popular api call

--- src/app/MostPopularNews/Listing/index.tsx
+++ src/app/MostPopularNews/Listing/index.tsx
@@ -1,30 +1,66 @@
+import { useIsFetching } from "@tanstack/react-query";
+import { useMemo } from "react";
+import { useGetMostPopular } from "../../../api/mostPopular/mostPopular.query";
+import { QUERY_KEYS } from "../../../api/queryKeys";
 import Card from "../../../components/ui/card";
+import Loading from "../../../components/ui/loading";
+import NoDataFound from "../../../components/ui/no-data-found";
 import Wrapper from "../../../components/ui/wrapper";
 
 const Listing = () => {
-  /**
-   * TODO: need to make api call
-   */
+  const isFetching = useIsFetching({
+    queryKey: [QUERY_KEYS.MOST_POPULAR],
+  });
+  const { data } = useGetMostPopular();
+  const renderCardList = useMemo(() => {
+    if (!data?.results?.length) {
+      return [];
+    }
+    const { results } = data;
+    return results.map((item) => {
+      const { abstract, byline, id, media, title, url } = item;
+      const firstMedia = media?.[0];
+      const imageData = firstMedia?.["media-metadata"]?.[1] || {};
+
+      return {
+        abstract,
+        title,
+        byline,
+        id: id.toString(),
+        media: imageData,
+        url,
+      };
+    });
+  }, [data]);
+
+  if (isFetching > 0) {
+    return <Loading />;
+  }
+
   return (
     <Wrapper className="flex flex-wrap gap-8">
-      {Array(10)
-        .fill(0)
-        .map(() => (
-          <Card
-            title="Hello world is best title"
-            id="100000009918397"
-            // adxkeyword key should be used
-            keywords={"Government Employees;Workplace Environment;United States".split(
-              ";"
-            )}
-            abstract="abstract abstract"
-            byline="byline"
-            media={{
-              url: "https://picsum.photos/id/237/400/300",
-              altText: "Sunset in the mountains",
-            }}
-          />
-        ))}
+      {renderCardList ? (
+        renderCardList.map(({ title, byline, id, abstract, url, media }) => (
+          <a
+            href={url}
+            target="_blank"
+            rel="noopener noreferrer"
+            key={`most-popular-card-list-${id}`}
+          >
+            <Card
+              title={title}
+              abstract={abstract}
+              byline={byline}
+              media={{
+                ...media,
+                altText: title,
+              }}
+            />
+          </a>
+        ))
+      ) : (
+        <NoDataFound />
+      )}
     </Wrapper>
   );
 };
