54bc70838fed3a7d6252ac3b8d01d727cb6934f4
jaypoojara <jaypoojara@gmail.com>
2025-01-13 18:53:50 +0530

    feat: add loading and no data found component

---
+++ src/components/ui/loading/index.tsx
@@ -0,0 +1,20 @@
+import { cn } from "../../../helpers/utils";
+
+type Props = {
+  className?: string;
+};
+
+const Loading = ({ className }: Props) => {
+  return (
+    <div
+      className={cn([
+        "fixed inset-0 bg-black/60 backdrop-blur-sm flex items-center justify-center",
+        className,
+      ])}
+    >
+      <div className="border-8 border-transparent rounded-full p-8 border-y-white animate-spin ease-in-out"></div>
+    </div>
+  );
+};
+
+export default Loading;
---
+++ src/components/ui/no-data-found/index.tsx
@@ -0,0 +1,9 @@
+const NoDataFound = () => {
+  return (
+    <div className="text-xl text-center font-semibold w-full">
+      No Data Found
+    </div>
+  );
+};
+
+export default NoDataFound;
